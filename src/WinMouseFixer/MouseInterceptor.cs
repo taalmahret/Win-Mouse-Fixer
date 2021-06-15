ea209e95ccae6894755193059412eceab0730d28
Ta'al Mahret <davidtawater@gmail.com>
2021-06-15 15:00:29 -0700

    Update MouseInterceptor.cs
    Added middle mouse button clicks

--- src/WinMouseFixer/MouseInterceptor.cs
+++ src/WinMouseFixer/MouseInterceptor.cs
@@ -93,6 +93,8 @@ namespace WinMouseFixer
             WM_LBUTTONDOWN = 0x0201,
             WM_LBUTTONUP   = 0x0202,
             WM_MOUSEMOVE   = 0x0200,
+            WM_MBUTTONDOWN = 0x0207,
+            WM_MBUTTONUP   = 0x0208,
             WM_MOUSEWHEEL  = 0x020A,
             WM_RBUTTONDOWN = 0x0204,
             WM_RBUTTONUP   = 0x0205
